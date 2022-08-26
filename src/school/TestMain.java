17f30fed9a6e02b70a2258cc22304debffe89b99
HOME <jeonghj27@naver.com>
2022-08-26 23:21:32 +0900

    22/08/26 학점산출프로그램

---
+++ src/school/TestMain.java
@@ -0,0 +1,32 @@
+package school;
+
+public class TestMain {
+	
+	Subject korean;
+	Subject math;
+
+	public static void main(String[] args) {
+		
+	}
+	
+	public void creatSubject() {
+		// 테스트 학생 생성
+		Student student1 = new Student(181213, "이정후", korean);
+		Student student2 = new Student(181518, "요키시", math);
+		Student student3 = new Student(171230, "김재웅", korean);
+		Student student4 = new Student(171255, "노운현", korean);
+		Student student5 = new Student(171590, "김시앙", math);
+		
+		//
+		
+		
+		
+	}
+	
+	
+	
+	
+	
+	
+	
+}
