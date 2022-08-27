d191c21c9810adbc101fbcde96547ed4f68146fc
HOME <jeonghj27@naver.com>
2022-08-27 21:58:22 +0900

    22/08/27 학점

--- src/school/GenerateGradeReport.java
+++ src/school/GenerateGradeReport.java
@@ -1,9 +1,23 @@
 package school;
 
-
+import java.util.ArrayList;
 
 public class GenerateGradeReport {
 	
+	School school = School.getInstance();
+	public static final String TITLE = " 수강생 학점 \t\t\n";
+	public static final String HEADER = "이름 | 학번 |필수과목| 점수  \n";
+	public static final String LINE = "---------------------------------\n";
 	
+	private StringBuffer buffer = new StringBuffer();
 	
+	public String getReport() {
+		ArrayList<Subject> subjectList = school.getSubjectList();
+		
+		return buffer.toString();
+	}
+	
+	public void makeHeader(Subject subject) {
+	//	buffer.append();
+	}
 }
